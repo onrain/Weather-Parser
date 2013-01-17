24a98364214eceec1bbe22afb7b088b772524cbb
SergeySlavin <prostirchas@gmail.com>
2013-01-17 14:21:05 +0200

    Initial Commit

---
+++ proj7/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  proj7
+//
+//  Created by Admin on 1/17/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
